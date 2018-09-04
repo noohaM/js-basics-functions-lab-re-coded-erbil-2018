9db023836c0c0edecaca5358a6e93394a927e09c
nooha mohammed <noohamohammed00@gmail.com>
2018-09-04 09:42:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block){
+  
+  if(block > 42)
+  {
+    return block-42;
+  }
+  else
+  {
+    return 42-block;
+  }
+}
+
+function distanceFromHqInFeet(feet)
+{
+  
+  return distanceFromHqInFeet(feet)*264;
+}
